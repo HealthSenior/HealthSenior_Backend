c47487626ecc44f8b23990d3dd3978bc856fc952
Chanho lee <devchlee12@gmail.com>
2024-04-03 21:48:56 +0900

    [FEAT] SeniorUser, CommunityChatRelation 도메인 추가

--- src/main/java/out4ider/healthsenior/domain/CommunityChatRelation.java
+++ src/main/java/out4ider/healthsenior/domain/CommunityChatRelation.java
@@ -1,4 +1,14 @@
 package out4ider.healthsenior.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class CommunityChatRelation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long communityChatRelationId;
+    @ManyToOne
+    SeniorUser seniorUser;
+    @ManyToOne
+    CommunityChatRoom communityChatRoom;
 }
--- src/main/java/out4ider/healthsenior/domain/SeniorUser.java
+++ src/main/java/out4ider/healthsenior/domain/SeniorUser.java
@@ -1,4 +1,18 @@
 package out4ider.healthsenior.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
 public class SeniorUser {
+    @Id
+    Long userId;
+    @OneToMany(mappedBy = "senior_user")
+    List<CommunityChatRelation> communityChatRelation;
+    String userName;
+    Integer userAge;
+    boolean isMale;
 }
