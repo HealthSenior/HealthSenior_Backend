449166c8a462b84dfd6ebe31bdd49c5f40db3122
Chanho lee <devchlee12@gmail.com>
2024-04-03 21:13:40 +0900

    [Feat] 프로젝트 시작

---
+++ src/main/java/out4ider/healthsenior/HealthseniorApplication.java
@@ -0,0 +1,13 @@
+package out4ider.healthsenior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthseniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthseniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/out4ider/healthsenior/HealthseniorApplicationTests.java
@@ -0,0 +1,13 @@
+package out4ider.healthsenior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthseniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
