0ca56627365fc3a4e8e2db741013f93c8dd042af
rosoa0475 <rosoa0475@gmail.com>
2024-04-03 22:50:14 +0900

    [FEAT] CommunityChatRoom 도메인 추가

--- src/main/java/out4ider/healthsenior/domain/CommunityChatRoom.java
+++ src/main/java/out4ider/healthsenior/domain/CommunityChatRoom.java
@@ -1,4 +1,39 @@
 package out4ider.healthsenior.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
 public class CommunityChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long chatRoomId;
+
+    @OneToMany(mappedBy = "communityChatRoom", cascade = CascadeType.REMOVE)
+    private List<CommunityChatRelation> CommunityChatRelation;
+
+    @Column
+    private Integer maxUserCount;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private String centerName;
+
+    @Column
+    private Long masterId;
+
+    @Column
+    private String sportKind;
+
+    @Column
+    private LocalDate startDate;
 }
