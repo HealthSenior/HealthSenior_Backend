90360879cb356366e5e81b873d9e0f4e53d3a236
Chanho lee <devchlee12@gmail.com>
2024-04-05 22:53:37 +0900

    [TEST] AWS서버 연결 테스트

--- src/main/java/out4ider/healthsenior/controller/ChatController.java
+++ src/main/java/out4ider/healthsenior/controller/ChatController.java
@@ -6,6 +6,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import out4ider.healthsenior.dto.ChatRequest;
 
 @RequiredArgsConstructor
@@ -16,5 +18,11 @@ public class ChatController {
     @SendTo("/subscribe_room/{chatRoomId}")
     public ChatRequest chat(@DestinationVariable Long chatRoomId, ChatRequest chatRequest){
         return chatRequest;
+    }
+
+    @ResponseBody
+    @GetMapping("/connection_test")
+    public String test(){
+        return "ok";
     }
 }
