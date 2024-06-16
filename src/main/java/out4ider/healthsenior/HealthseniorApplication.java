9a3ac3f03cd08ffcb0b633dd14d10315cd4b5a11
Chanho lee <devchlee12@gmail.com>
2024-06-16 18:12:11 +0900

    [FIX] RabbitMQ Exchange설정 안되는 문제 해결

--- src/main/java/out4ider/healthsenior/HealthseniorApplication.java
+++ src/main/java/out4ider/healthsenior/HealthseniorApplication.java
@@ -1,7 +1,9 @@
 package out4ider.healthsenior;
 
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
@@ -9,7 +11,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class HealthseniorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthseniorApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(HealthseniorApplication.class, args);
+		RabbitAdmin rabbitAdmin = context.getBean(RabbitAdmin.class);
+		rabbitAdmin.initialize();
 	}
 
 }
